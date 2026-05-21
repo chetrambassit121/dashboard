8c5059c9adf387747daf34fd9b821cac5b9b05bf
Chetram Bassit <chetrambassit@Mac.lan>
2026-05-21 14:05:12 -0400

    get Django Database Data to the React JS Frontend

--- frontend/src/components/Dashboard1.jsx
+++ frontend/src/components/Dashboard1.jsx
@@ -1,6 +1,22 @@
-import react from 'react'
+import { React, useState, useEffect } from 'react'
+import AxiosInstance from './Axios'
 
 const Dashboard1 = () => {
+
+    const [myData, setMyData] = useState([])
+    console.log('myData: ', myData)
+
+    const GetData = async () => {
+        AxiosInstance.get(`supermarketsales/`)
+        .then((res) => {
+            setMyData(res.data)
+        })
+    }
+
+    useEffect(() => {
+        GetData()
+    }, [])
+
     return(
         <div>This is the Dashboard1</div>
     )
