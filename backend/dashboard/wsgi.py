d8574b3d6d7d623e2df3beec078d774f5be38721
Chetram Bassit <chetrambassit@Mac.lan>
2026-06-08 11:41:51 -0400

    Serve Vite dist with WhiteNoise

--- backend/dashboard/wsgi.py
+++ backend/dashboard/wsgi.py
@@ -7,10 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+
+# application = get_wsgi_application()
+
+
+
+
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard.settings")
 
 application = get_wsgi_application()
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+application = WhiteNoise(application)
+application.add_files(BASE_DIR / "frontend" / "dist", prefix="static/")
